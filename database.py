979090f55d05f4bbf50170402e38dfc1a89ab0c6
Dabeey <abbasdaughter18@gmail.com>
2025-09-23 14:44:07 +0100

    feat(database): Implement asynchronous database connection and session management

--- database.py
+++ database.py
@@ -1,4 +1,28 @@
 from sqlalchemy.orm import sessionmaker,  DeclarativeBase
-from sqlalchemy.ex import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+import os
+
+DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL')
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+Base = DeclarativeBase()
+
+async def get_db():
+    async with AsyncSessionLocal() as db:
+        try:
+            yield db
+        finally:
+            db.close()
 
 
+def create_table():
+    Base.metadata.create_all(bind=engine)
